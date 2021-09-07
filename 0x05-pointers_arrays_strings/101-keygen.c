5cb08ac183403f7d0d34ac7041720201cedb375a
Bisrat <bisryawaye@gmail.com>
2021-09-07 10:25:19 -0700

    correcting 10 and 10

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,34 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include <string.h>
+
 /**
+ * main - the program generate random 9 digits
  *
- *
+ * Return: 0 if successful
  */
+
 int main(void)
 {
+int i = 0;
+int n = 0;
+int sum = 0;
+char password[100];
 
-srand(time(0));
-int num = rand();
-printf("%i\n",num);
+char al[63] = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+srand(time(NULL));
+while (sum < (2772 - 122))
+{
+n = rand() % 62;
+password[i] = al[n];
+sum = sum + password[i];
+i++;
+}
+
+n = 2772 - sum;
+password[i] = n;
+password[++i] = '\0';
+printf("%s\n", password);
 return (0);
 }
